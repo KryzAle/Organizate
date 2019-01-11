e2483b72ad73f565c960d4ed200b931ba07dec13
Unknown <kryzale@gmail.com>
2019-01-10 23:28:22 -0500

    Login con Fb implementado

---
+++ Organizate/app/src/main/java/com/kryzcorp/kryzale/organizate/MyApp.java
@@ -0,0 +1,16 @@
+package com.kryzcorp.kryzale.organizate;
+
+import android.app.Application;
+
+import com.facebook.appevents.AppEventsLogger;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AppEventsLogger.activateApp(this);
+    }
+}
+
