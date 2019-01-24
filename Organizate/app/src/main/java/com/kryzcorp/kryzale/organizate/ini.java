eba2516d3543ef824a5fd502e7e91d85b3cfa171
Unknown <kryzale@gmail.com>
2019-01-24 17:06:33 -0500

    Validacion de Loggeo con FB

--- Organizate/app/src/main/java/com/kryzcorp/kryzale/organizate/ini.java
+++ Organizate/app/src/main/java/com/kryzcorp/kryzale/organizate/ini.java
@@ -8,6 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
 
 public class ini extends AppCompatActivity {
 
@@ -17,10 +21,36 @@ public class ini extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ini);
-
+        Button btnRegistrarse = findViewById(R.id.btnRegistrame);
+        Button btnIncioSesion = findViewById(R.id.btnIniciaSesion);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if (isLoggedIn){
+            goMainScreen();
+        }
+
+    }
+    public void LanzarLogin(View view){
+
+
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivity(i);
+
+    }
+    public void LanzarRegistro(View view){
+
+
+        Intent i = new Intent(this,registro.class);
+        startActivity(i);
+
+    }
+    private void goMainScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 }
