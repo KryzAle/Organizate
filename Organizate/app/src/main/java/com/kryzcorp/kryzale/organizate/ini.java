d995c472c6fef0c2be5a1831c84dddbc3d4c1351
Unknown <kryzale@gmail.com>
2019-01-24 14:09:21 -0500

    Configurada la activity inicial

--- Organizate/app/src/main/java/com/kryzcorp/kryzale/organizate/ini.java
+++ Organizate/app/src/main/java/com/kryzcorp/kryzale/organizate/ini.java
@@ -18,33 +18,9 @@ public class ini extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ini);
 
-        btnlogin=findViewById(R.id.btnIniciarsesion);
-         btnsignup=findViewById(R.id.btnRegistrar);
-
-         btnsignup.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent login=new Intent(ini.this, LoginActivity.class);
-             }
-         });
-         btnsignup.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent sign=new Intent(ini.this,registro.class)
-             }
-         });
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
